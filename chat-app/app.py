8223e611dcac26d617c61d08d12121395a292b4c
eifion14 <eifionowen14@gmail.com>
2022-02-16 15:18:43 +0000

    small change to login
    linked login to register page

--- chat-app/app.py
+++ chat-app/app.py
@@ -5,9 +5,16 @@ from datetime import datetime
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+
 @app.route('/register')
 def register():
     return render_template('register.html')
+
 
 @app.route('/home')
 def home():
