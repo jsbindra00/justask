ee682634ffe8a8d4fca3d3d63e804aef57149550
Ahmed Shaaban <ashaaban0109@gmail.com>
2022-03-21 15:25:24 +0000

    cleanup

--- Testing Area/chat-app/test_app.py
+++ Testing Area/chat-app/test_app.py
@@ -9,30 +9,6 @@ import pytest
 
 
 class MyTestCase(unittest.TestCase):
-    # API_URL = "127.0.0.1:5000/"
-
-    @pytest.fixture()
-    def app():
-        app = create_app()
-        app.config.update({
-            "TESTING": True,
-        })
-
-        # other setup can go here
-
-        yield app
-
-        # clean up / reset resources here
-
-
-    @pytest.fixture()
-    def client(app):
-        return app.test_client()
-
-
-    @pytest.fixture()
-    def runner(app):
-        return app.test_cli_runner()
 
     def test_request_example(client):
         response = client.get("/posts")
