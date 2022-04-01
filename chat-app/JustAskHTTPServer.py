1f7feaca45b4ee55287b677cf674f67961e299d8
n1ghtrid3r <60359484+n1ghtrid3r@users.noreply.github.com>
2022-04-02 00:15:50 +0100

    refactoring backend

--- chat-app/JustAskHTTPServer.py
+++ chat-app/JustAskHTTPServer.py
@@ -29,27 +29,35 @@ class JustAskHTTPServer(FlaskView):
         if updateDB:
             # if we just replaced the username in our session, perform search by email.
             SEARCH_PRED = ClientAttribute(ClientAttribute.EMAIL if client_attribute == ClientAttribute.USERNAME else ClientAttribute.USERNAME)
-            print(session[SEARCH_PRED.name])
             current_user = ClientModel.query.filter_by(**{SEARCH_PRED.name : session[SEARCH_PRED.name]}).first()
             setattr(current_user, client_attribute.name, arg)
             if auto_commit: db.session.commit()
+    def GetUser(self, client_attributes):
+        return ClientModel.query.filter_by(**client_attributes).first()
 
     def GetSessionInformation(self, client_attribute):
         return session[ClientAttribute(client_attribute).name]
 
-
-    def KickUser(self, data):
-        
-        pass
+    # This should be on SocketServer.py
+    def KickUser(self, username):
+        user = self.GetUser({ClientAttribute.USERNAME.name : username})
+        if user is not None:
+            user.ACTIVE_SESSION = None
+            user.ADMIN = 0
+            
+        return redirect("/profile")
     
     def IsUserLoggedIn(self):
         return session
     
-    def CreateUser(self):
-        pass
+    def CreateUser(self, user_information):
+        user = ClientModel(**user_information)
+        db.session.add(user)
+        db.session.commit()
+        return user
 
-    def UserExists(self):
-        pass
+    def UserExists(self, username, email):
+        return ClientModel.query.filter_by(EMAIL = email).first() != None or ClientModel.query.filter_by(USERNAME = username).first() != None
 
     @route("/signout", endpoint="signout")
     def ROUTE_SIGNOUT(self):
@@ -61,12 +69,11 @@ class JustAskHTTPServer(FlaskView):
     def ROUTE_LANDING_PAGE(self):
         return redirect("profile") if self.IsUserLoggedIn() else render_template("landingpage.html")
 
-    @route("/leave_session", endpoint="leave_session",methods=["POST"])
+    @route("/leave_session", endpoint="leave_session",methods=["POST", "GET"])
     def ROUTE_LEAVE_SESSION(self):
-        if session["ACTIVE_SESSION"]: self.UpdateSessionInformation(ClientAttribute.ACTIVE_SESSION, None, updateDB=True)
+        if session["ACTIVE_SESSION"]: self.UpdateSessionInformation(ClientAttribute.ACTIVE_SESSION, "", updateDB=True)
         return redirect("/profile")
         
-
 
     def CHANGE_PROFILE_INFORMATION(self, default_args):
 
@@ -108,34 +115,17 @@ class JustAskHTTPServer(FlaskView):
     @route("/profile", endpoint="profile",methods=["GET", "POST"])
     def ROUTE_PROFILE(self):
         if not self.IsUserLoggedIn(): return render_template("landingpage.html")
-        default_args = {"FIRSTNAME" : session["FIRSTNAME"], "LASTNAME" : session["LASTNAME"], "EMAIL": session["EMAIL"], "PASSWORD": session["PASSWORD"], "USERNAME": session["USERNAME"]}
-
-
-        print(default_args)
+        default_args = {key.name : session[key.name] for key in ClientAttribute}
+        # default_args = {"FIRSTNAME" : session["FIRSTNAME"], "LASTNAME" : session["LASTNAME"], "EMAIL": session["EMAIL"], "PASSWORD": session["PASSWORD"], "USERNAME": session["USERNAME"]}
         if request.method == "GET": return render_template("profile.html", **default_args)
         if "submit-profile" in request.form: default_args = self.CHANGE_PROFILE_INFORMATION(default_args)
         elif "submit-password" in request.form: default_args = self.CHANGE_PASSWORD_INFORMATION(default_args)
         try: db.session.commit()
-        except Exception as e:
-            print(e)
+        except Exception as e: print(e)
         return render_template("profile.html", **default_args)
 
-        
-    @route("/", endpoint="/")
-    @route("/login/", endpoint="login", methods=['POST', 'GET'])
-    def ROUTE_LOGIN(self):
-        if request.method == "GET":
-            if session: return redirect("landingpage")
-        form_email = request.form.get("email")
-        form_password = Utility.EncryptSHA256(request.form.get("password"))
-        if not form_email or not form_password: return render_template("login.html")
-
-        user = ClientModel.query.filter_by(EMAIL=form_email, PASSWORD=form_password).first()
-        if  user == None:
-            #todo handle this. Invalid login credentials.
-            print("INVALID LOGIN CREDENTIALS")
-            return render_template("landingpage.html")
-        # optimise
+    
+    def LOGIN_CONFIRMATION(self, user):
         self.UpdateSessionInformation(ClientAttribute.EMAIL, user.EMAIL)
         self.UpdateSessionInformation(ClientAttribute.USERNAME, user.USERNAME)
         self.UpdateSessionInformation(ClientAttribute.FIRSTNAME, user.FIRSTNAME)
@@ -145,79 +135,75 @@ class JustAskHTTPServer(FlaskView):
         self.UpdateSessionInformation(ClientAttribute.ADMIN, user.ADMIN)
 
         return redirect("/profile")
+        
+
+    @route("/", endpoint="/")
+    @route("/login/", endpoint="login", methods=['POST', 'GET'])
+    def ROUTE_LOGIN(self):
+        if request.method == "GET" and not self.IsUserLoggedIn(): return redirect("landingpage")
+        form_email = request.form.get("email")
+        form_password = Utility.EncryptSHA256(request.form.get("password"))
+        if not form_email or not form_password: return render_template("login.html")
+        user = self.GetUser({ClientAttribute.EMAIL.name : form_email, ClientAttribute.PASSWORD.name : form_password})
+        if  user == None:
+            print("INVALID LOGIN CREDENTIALS")
+            return render_template("landingpage.html")
+        return self.LOGIN_CONFIRMATION(user)
+
 
     @route("/registration/",endpoint="registration", methods = ["GET", "POST"])
     def ROUTE_REGISTRATION(self):
         
         if self.IsUserLoggedIn() and request.method == "GET": return redirect("/profile")
-        
-        
-        form_email = request.form.get("email")
-        form_username = request.form.get("username")
-        form_first_name = request.form.get("firstname")
-        form_last_name = request.form.get("lastname")
-        form_password = Utility.EncryptSHA256(request.form.get("password"))
-        active_session = "0"
-        
-        data = [form_email, form_username,form_first_name,form_last_name,form_password, active_session]
-        for field in data:
-            if not field:
-                #todo handle this
+        new_user_details = {
+            ClientAttribute.EMAIL.name : request.form.get("email"), 
+            ClientAttribute.USERNAME.name : request.form.get("username"), 
+            ClientAttribute.FIRSTNAME.name : request.form.get("firstname"), 
+            ClientAttribute.LASTNAME.name : request.form.get("lastname"), 
+            ClientAttribute.PASSWORD.name : Utility.EncryptSHA256(request.form.get("password")),
+            ClientAttribute.ACTIVE_SESSION.name : None,
+            ClientAttribute.ADMIN.name : 0 
+            }
+        for key,value in new_user_details.items():
+            if value is None:
                 return "404"
 
-
-
-        user_exists = ClientModel.query.filter_by(EMAIL=form_email).first()
-        if user_exists != None :
-            #todo handle this. User is already registered, initiate a popup.
-            return redirect("/landingpage")
-
-        db.session.add(ClientModel(USERNAME = form_username, FIRSTNAME = form_first_name, LASTNAME = form_last_name, EMAIL = form_email, PASSWORD = form_password, ACTIVE_SESSION = "0", ADMIN = 0))
-        db.session.commit()
-
-        return redirect("/profile")
-
-    @route("/chat_logout/", endpoint="chat_logout")
-    def chat_logout(self):
-        self.UpdateSessionInformation(ClientAttribute.ACTIVE_SESSION, "")
-        return redirect("/session")
+ 
+        if self.UserExists(username=new_user_details[ClientAttribute.USERNAME.name], email=new_user_details[ClientAttribute.EMAIL.name]): redirect("/landingpage")
+        user = self.CreateUser(new_user_details)
+        return self.LOGIN_CONFIRMATION(user)
 
     @route("/logout", endpoint="logout")
-    def logout():
+    def ROUTE_LOGOUT():
         session.clear()
         return redirect("login")
 
     @route("/session", endpoint="session", methods=["GET", "POST"])
     def ROUTE_MANAGE_SESSIONS(self):
-        if request.method == "GET":
-            if self.GetSessionInformation(ClientAttribute.ACTIVE_SESSION) != None:
-                return redirect(url_for("chat"))
-            return render_template("session.html")
-        roomID = request.form.get("room")
-        matchingRoomClients = ClientModel.query.filter_by(ACTIVE_SESSION = roomID).all()
-        
-        if "joinsession" in request.form:
+        if request.method == "POST":
+            roomID = request.form.get("room")
             matchingRoomClients = ClientModel.query.filter_by(ACTIVE_SESSION = roomID).all()
+            
+            if "joinsession" in request.form:
+                matchingRoomClients = ClientModel.query.filter_by(ACTIVE_SESSION = roomID).all()
 
-            if matchingRoomClients == []:
-                # handle this.
-                return "room id does not exist"
-        
-        elif "createsession" in request.form:
-            if matchingRoomClients != []:
-                return "session id already exists"
+                if matchingRoomClients == []:
+                    # handle this.
+                    return "room id does not exist"
+            elif "createsession" in request.form:
+                if matchingRoomClients != []:
+                    return "session id already exists"
 
-        self.UpdateSessionInformation(ClientAttribute.ACTIVE_SESSION, roomID, updateDB=True)
+            self.UpdateSessionInformation(ClientAttribute.ACTIVE_SESSION, roomID, updateDB=True)
         return redirect(url_for("chat"))
 
-    @route("/chat", endpoint="chat")
+    @route("/chat", endpoint="chat",methods=["GET"])
     def ROUTE_CHAT_SYSTEM(self):
-        username = session.get('USERNAME')
-        room = session.get('ACTIVE_SESSION')
-        if username and room:
-            return render_template('chat.html', username=username, room=room)
-        else:
-            return redirect(url_for('session'))
+        if not self.IsUserLoggedIn(): return redirect("landingpage")
+        username = self.GetSessionInformation(ClientAttribute.USERNAME)
+        active_session = self.GetSessionInformation(ClientAttribute.ACTIVE_SESSION)
+        if active_session: return render_template("chat.html", username=username, room=active_session)
+        return render_template("session.html")
 
     @route("/sketchpad", endpoint="sketchpad", methods=["GET", "POST"])
     def ROUTE_SKETCHPAD(self):
