785f8f0d4c1ba7d7cff1c98b30d9c5987796e6e2
n1ghtrid3r <60359484+n1ghtrid3r@users.noreply.github.com>
2022-04-04 01:54:24 +0100

    fixed sort button hooks on chat system

---
+++ chat-app/Client.py
@@ -0,0 +1,48 @@
+from SharedContext import db
+from ModelBase import ModelBase
+from enum import IntEnum
+
+class ClientAttribute(IntEnum):
+    EMAIL=0
+    USERNAME=1
+    FIRSTNAME=2
+    LASTNAME=3
+    PASSWORD=4
+    ACTIVE_SESSION=5
+    ADMIN=6
+    PROFILE_PICTURE = 7
+    INSTAGRAM_PAGE=8
+    TWITTER_PAGE=9
+    FACEBOOK_PAGE=10
+    LINKEDIN_PAGE=11
+
+class ClientModel(db.Model, ModelBase):
+
+    __bind_key__ = "clients"
+
+    EMAIL = db.Column(db.String(120), unique=True, nullable=False, primary_key=True)
+    USERNAME = db.Column(db.String(80), unique=True, nullable=False)
+    FIRSTNAME = db.Column(db.String(80), unique=False, nullable=False)
+    LASTNAME = db.Column(db.String(80), unique=False, nullable=False)
+    PASSWORD = db.Column(db.String(80), unique=False, nullable=False)
+    ACTIVE_SESSION = db.Column(db.String(80), unique=False, nullable=False)
+    ADMIN = db.Column(db.Integer, unique=False, nullable=False)
+
+    PROFILE_PICTURE = db.Column(db.String(80), unique=False, nullable=False)
+    INSTAGRAM_PAGE = db.Column(db.String(80), unique=False, nullable=False)
+    TWITTER_PAGE = db.Column(db.String(80), unique=False, nullable=False)
+    FACEBOOK_PAGE = db.Column(db.String(80), unique=False, nullable=False)
+    LINKEDIN_PAGE = db.Column(db.String(80), unique=False, nullable=False)
+
+
+    def __repr__(self):
+        return 'CLIENT {} {} {} {} {} {}'.format(self.email, self.username, self.firstname, self.lastname, self.password, self.active_session)
+    def __init__(self, **kwargs):
+        super(ClientModel, self).__init__(**kwargs)
+
+    
+    def SaveDatabase(filePath):
+        # pull all clients.
+        ModelBase.ProcessDatabase(ClientModel.query.all(), filePath)
+        print("SAVED CLIENTS DB TO {}".format(filePath))
+    
---
+++ chat-app/Message.py
@@ -0,0 +1,49 @@
+from enum import IntEnum
+from ModelBase import ModelBase
+
+
+from SharedContext import db
+from enum import IntEnum
+
+
+
+class MessagePacket(IntEnum):
+    MESSAGE_ID = 0
+    MESSAGE_FLAIRS = 1
+    DATE_SENT = 2
+    NUM_UPVOTES = 3
+    PAYLOAD = 4
+    FROM_SESSION_ID = 5
+    FROM_USER = 6
+
+
+class MessageModel(db.Model):
+    __bind_key__ = "messages"
+
+    message_id = db.Column(db.String(120), unique=False, nullable=False, primary_key=True)
+    message_flairs = db.Column(db.String(120), unique=False, nullable=False, primary_key=False)
+    date_sent = db.Column(db.String(120), unique=False, nullable=False, primary_key=False)
+    num_upvotes = db.Column(db.Integer, unique=False, nullable=False, primary_key=False)
+    payload = db.Column(db.String(120), unique=False, nullable=False, primary_key=False)
+    from_session_id = db.Column(db.String(120), unique=False, nullable=False, primary_key=False)
+    from_user = db.Column(db.String(120), unique=False, nullable=False, primary_key=False)
+
+    def __repr__(self):
+        return 'MESSAGE {} {} {} {} {} {} {}'.format(self.message_id, self.message_flairs, self.date_sent, self.num_upvotes, self.payload, self.from_session_id, self.from_user) 
+    def __init__(self, **kwargs):
+        super(MessageModel, self).__init__(**kwargs)
+
+    def SaveDatabase(filePath):
+        # pull all clients.
+        ModelBase.ProcessDatabase(MessageModel.query.all(), filePath)
+        print("SAVED MESSAGES DB TO {}".format(filePath))
+
+
+
+
+
+
+
+
+
+
